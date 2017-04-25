c376b772186d1b64f9a79ed57a11fa09ed1cebd9
adam <adam@marczuk.info>
2017-04-25 15:27:33 +0200

    fix cli

--- bin/phresto.php
+++ bin/phresto.php
@@ -1,8 +1,8 @@
 <?php
 
-define( 'PHRESTO_ROOT', __DIR__ . '/../../' );
+define( 'PHRESTO_ROOT', __DIR__ . '/../../../../../' );
 
-require_once __DIR__ . '/../autoload.php';
+require_once __DIR__ . '/../../../../autoload.php';
 
 $functions = [
 	'--updatemodulesiniulesini' => [ 'func' => 'updatemodules', 'desc' => 'Updates/creates conf/modules.ini' ],
